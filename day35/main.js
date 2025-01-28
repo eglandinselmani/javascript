c8f5d8213c03fc3d0f4c45a8d6573c0a64464a2d
eglandinselmani <eglandinselmani1@gmail.com>
2025-01-28 20:49:51 +0100

    day35

--- day35/main.js
+++ day35/main.js
@@ -1,48 +1,30 @@
-const circle = document.getElementById('circle');
-const timerDisplay = document.getElementById('timer');
+var slideIndex= 1;
 
-let startTime;
-let endTime;
-let elapsedTime = 0;
+function showslides(){
+    var i;
+    var slides; document.getElementsById ("slide");
 
-circle.style.position = 'absolute';
-circle.style.left = '50px';
-circle.style.top = '50px';
+    for( i=0 ; i<slide.length; i++){
+        slide=[i].style.display ="none";
+    }
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+
+    if(n> slides.length){
+        slideIndex=1;
+    }
+
+    if(n<1){
+        slideIndex = slideIndex
+    }
+    slides=[slideIndex-1].style.display = "block";
 }
 
-circle.addEventListener('click', () => {
-  const windowWidth = window.innerWidth;
-  const windowHeight = window.innerHeight;
+showslides(1);
 
-  const randomX = Math.random() * (windowWidth - 150);
-  const randomY = Math.random() * (windowHeight - 150);
+function plusSlides(n){
+    slideIndex += n;
+    showslides(slideIndex);
+}
 
-  circle.style.left = `${randomX}px`;
-  circle.style.top = `${randomY}px`;
+setInterval(plusSlides, 3000, 1);
 
-  // Change the color of the circle on each click
-  circle.style.backgroundColor = getRandomColor();
-
-  if (!startTime) {
-    startTime = Date.now();
-    timerDisplay.textContent = `Timer started! Click again to stop.`;
-  } else {
-    endTime = Date.now();
-    elapsedTime = endTime - startTime;
-    const seconds = (elapsedTime / 1000).toFixed(2);
-    const milliseconds = elapsedTime % 1000;
-    
-    timerDisplay.textContent = `Click time: ${seconds} seconds (${milliseconds} ms)`;
-
-    // Reset start time for the next click
-    startTime = null;
-  }
-});
